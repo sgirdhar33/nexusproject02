8bfcda9283ba50d3b1ff5b0d21ee42468f106619
sgirdhar33 <49391388+sgirdhar33@users.noreply.github.com>
2022-01-09 21:12:08 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('compress', function() {
-  return gulp.src("dist/mindfire/**").pipe(zip("mindfire.zip"))
+  return gulp.src("dist/MyFirstApp/**").pipe(zip("MyFirstApp.zip"))
     .pipe(gulp.dest('./nexus/nexus-repo'));
 });
